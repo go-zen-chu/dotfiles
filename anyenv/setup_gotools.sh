e88528ba99213ec4cd0c3b7d73426799a2b0b51d
Akira Masuda <masuda.akira1014@gmail.com>
2019-02-11 23:14:43 +0900

    add gotools

---
+++ anyenv/setup_gotools.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# test tools
+go get -u github.com/onsi/ginkgo/ginkgo
+go get -u github.com/onsi/gomega/...
+# opsmanager tool
+go get -u github.com/pivotal-cf/om
+
+go get -u github.com/krishicks/yaml-patch/cmd/yaml-patch
