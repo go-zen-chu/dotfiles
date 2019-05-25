efed210b915a59f6013574300df41518cb03a843
Akira Masuda <masuda.akira1014@gmail.com>
2019-05-25 23:31:29 +0900

    fix for centos

--- anyenv/setup_anyenv.sh
+++ anyenv/setup_anyenv.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+set -eux
+
 git clone https://github.com/riywo/anyenv ${HOME}/.anyenv
 # update path for now
-PATH=${HOME}/.anyenv/bin:$PATH
+PATH=${HOME}/.anyenv/bin:$PATH >> ${HOME}/local.zsh
 
-${HOME}/.anyenv/bin/anyenv init
+anyenv init
+anyenv install --force-init 
 
 anyenv install pyenv
 anyenv install goenv
