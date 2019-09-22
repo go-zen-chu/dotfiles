551ddad1176db12fae107bf7be5877ffcc8301a4
Akira Masuda <masuda.akira1014@gmail.com>
2019-09-22 17:33:15 +0900

    update

--- vim/setup.sh
+++ vim/setup.sh
@@ -13,16 +13,16 @@ case "${os}" in
 esac
 vim --version
 
-# install dein as plugin manager for vim
-if [[ ! -d "$HOME/.vim/dein" ]] ; then
-	curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o ${HOME}/.vim/installer.sh
-	sh ${HOME}/.vim/installer.sh ${HOME}/.vim/dein/
-fi
-
 # copy all things inside .vim
 if [[ ! -d "$HOME/.vim" ]] ; then
 	cp -r ./vim/.vim ${HOME}
 	chown -R ${USER} ${HOME}/.vim
+fi
+
+# install dein as plugin manager for vim
+if [[ ! -d "$HOME/.vim/dein" ]] ; then
+	curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o ${HOME}/.vim/installer.sh
+	sh ${HOME}/.vim/installer.sh ${HOME}/.vim/dein/
 fi
 
 cp -i ./vim/.vimrc ${HOME} # place .vimrc which loads settings in dotfiles
