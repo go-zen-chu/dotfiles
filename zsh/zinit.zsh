560ce6e7512d25c0f2e67e67247385ecd9ffd475
Akira Masuda <masuda.akira1014@gmail.com>
2020-04-06 09:45:01 +0900

    update

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -1,5 +1,14 @@
 export PATH="${HOME}/.zinit/bin:${PATH}"
 source ${HOME}/.zinit/bin/zinit.zsh
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zinit-zsh/z-a-patch-dl \
+    zinit-zsh/z-a-as-monitor \
+    zinit-zsh/z-a-bin-gem-node
 
 # alias
 zinit snippet 'OMZ::plugins/git/git.plugin.zsh'
