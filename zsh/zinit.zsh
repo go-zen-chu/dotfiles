b106bda1e7477134dca05abbe1015d0b00743a1e
Akira Masuda <masuda.akira1014@gmail.com>
2020-10-12 01:15:13 +0900

    fix zsh history search

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -36,12 +36,24 @@ zinit light 'zsh-users/zsh-autosuggestions'
 zinit ice from"gh-r" as"program"
 zinit load junegunn/fzf-bin
 export FZF_DEFAULT_OPTS='--height 60% --reverse --border'
-function select-history() {
-  BUFFER=$(\history -n -r 1 | fzf -e --no-sort +m --query "$LBUFFER" --prompt="History > ")
-  CURSOR=$#BUFFER
+# migrate from https://github.com/junegunn/fzf/blob/fc7630a66d8b07ec90603f7919f8aadf891783ac/shell/key-bindings.zsh#L106
+fzf-history-widget() {
+  local selected num
+  setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
+  selected=( $(fc -rl 1 | perl -ne 'print if !$seen{(/^\s*[0-9]+\**\s+(.*)/, $1)}++' |
+    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" fzf) )
+  local ret=$?
+  if [ -n "$selected" ]; then
+    num=$selected[1]
+    if [ -n "$num" ]; then
+      zle vi-fetch-history -n $num
+    fi
+  fi
+  zle reset-prompt
+  return $ret
 }
-zle -N select-history
-bindkey '^r' select-history
+zle     -N   fzf-history-widget
+bindkey '^r' fzf-history-widget
 
 # should load after compinit. https://github.com/zsh-users/zsh-syntax-highlighting#faq
 zinit light "zsh-users/zsh-syntax-highlighting"
