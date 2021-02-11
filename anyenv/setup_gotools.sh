229773ec0ef11311f078d9da58e678109b8a257a
Akira Masuda <masuda.akira1014@gmail.com>
2021-02-11 13:17:31 +0900

    update packages for mac

--- anyenv/setup_gotools.sh
+++ anyenv/setup_gotools.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-go get -u github.com/golang/mock/gomock
 # test tools
-go get -u github.com/onsi/ginkgo/ginkgo
-go get -u github.com/onsi/gomega/...
+go get -u github.com/golang/mock/gomock
+go get -u github.com/cweill/gotests/...
 
 # dev tool
-go get -u github.com/pivotal-cf/om
-go get -u github.com/krishicks/yaml-patch/cmd/yaml-patch
 go get -u github.com/krishicks/minio/mc
