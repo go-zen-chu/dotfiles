16227a29b3dc167ab97c77be32ac551920d47d29
Akira Masuda <masuda.akira1014@gmail.com>
2023-01-11 23:12:05 +0900

    [skip ci] add kubectl krew & krew packages

--- aqua/setup.sh
+++ aqua/setup.sh
@@ -5,11 +5,19 @@ curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.0.2/aqua
 # temporary add to PATH (add to PATH in zshrc for permanent configuration)
 export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua"
-cp ./aqua/aqua.yaml ${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml
+cp ./aqua/aqua.yaml "${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml"
 # set to global config
 export AQUA_GLOBAL_CONFIG=${AQUA_GLOBAL_CONFIG:-}:${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml
 aqua i -a
 # gh should be installed via aqua
 if hash gh 2>/dev/null; then
     gh config set editor vim
+fi
+# kubectl & krew should be installed via aqua
+if hash kubectl 2>/dev/null; then
+    kubectl krew install ctx \
+        ns \
+        access-matrix \
+        tree \
+        neat
 fi
