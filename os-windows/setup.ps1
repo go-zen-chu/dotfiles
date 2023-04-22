01c28af27eb4f21e28fa723bebf9491f8da30328
Akira Masuda <masuda.akira1014@gmail.com>
2023-04-22 11:22:09 +0900

    Update setup.ps1

--- os-windows/setup.ps1
+++ os-windows/setup.ps1
@@ -1,20 +1,25 @@
-Write-Host -ForgroundColor Cyan 'Setup for Windows...'
-# install basic tools
-winget install Google.Chrome
-winget install Google.Drive
-winget install Microsoft.PowerToys
-# communication tools
-winget install Zoom.Zoom
-winget install LINE.LINE
-winget install Mozilla.Thunderbird
-# other tools
-winget install Synology.DriveClient
-winget install Wondershare.Filmora
-winget install Doist.Todoist
-# get dnssd.dll not found error because of lack of Bonjour
-#winget install Apple.iTunes
+Write-Host "Setup for Windows..." -ForegroundColor Cyan 
+$packages = @(  
+    # install basic tools
+    "Google.Chrome",
+    "Google.Drive",
+    "Microsoft.PowerToys",
+    # communication tools
+    "Zoom.Zoom",
+    "LINE.LINE",
+    "Mozilla.Thunderbird",
+    "SlackTechnologies.Slack",
+    # other tools
+    "Synology.DriveClient",
+    "Wondershare.Filmora",
+    "Doist.Todoist",
+    # get dnssd.dll not found error because of lack of Bonjour
+    #"Apple.iTunes",
+    # development tools
+    "Microsoft.VisualStudioCode"
+)
+foreach ($package in $packages) {
+    winget install $package -e
+}
 
-# development tools
-winget install Microsoft.VisualStudioCode
-
-Write-Host -ForgroundColor Cyan 'Setup completed'
+Write-Host "Setup completed" -ForegroundColor Cyan 
