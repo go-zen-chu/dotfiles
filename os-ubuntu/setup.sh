b7747aa2aaa37545ec8d7e8295b50f0feb809ac6
Akira Masuda <masuda.akira1014@gmail.com>
2023-05-05 12:57:54 +0900

    add protobuf-compiler

--- os-ubuntu/setup.sh
+++ os-ubuntu/setup.sh
@@ -10,4 +10,6 @@ sudo dpkg-reconfigure locales
 sudo apt-get update
 # to enable cgo in golang, gcc is required
 sudo apt-get install gcc
+# development tools
+sudo apt install -y protobuf-compiler
 sudo apt-get clean
