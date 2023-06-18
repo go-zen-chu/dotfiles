4d0974bbb58e346a5407ba238247a2492348e47a
Akira Masuda <masuda.akira1014@gmail.com>
2023-06-19 01:03:55 +0900

    update

--- os-windows/README.md
+++ os-windows/README.md
@@ -1,5 +1,8 @@
 # Windows
 
 ```powershell
-PowerShell -ExecutionPolicy Unrestricted .\windows\setup.ps1
+cd \\wsl.localhost\Ubuntu\home\USER_HERE\dotfiles
+# required for loading profile.ps1
+Set-ExecutionPolicy RemoteSigned
+PowerShell -ExecutionPolicy Unrestricted .\os-windows\setup.ps1
 ```
--- os-windows/setup.ps1
+++ os-windows/setup.ps1
@@ -23,4 +23,16 @@ foreach ($package in $packages) {
     winget install $package -e
 }
 
+if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts)) {
+    Write-Host "Installing powershell profile if not exists" -ForegroundColor Cyan
+    New-Item -ItemType Directory -Force -Path "$PROFILE.CurrentUserAllHosts\.."
+    Copy-Item "os-windows\profile.ps1" -Force -Destination "$PROFILE.CurrentUserAllHosts\.."
+}
+
+$UserProperty = $(Get-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders')
+if (Test-Path -Path $UserProperty.Startup) {
+    Write-Host "Installing autohotkey script to startup" -ForegroundColor Cyan 
+    Copy-Item "os-windows\keybind.ahk" -Force -Destination $UserProperty.Startup
+}
+
 Write-Host "Setup completed" -ForegroundColor Cyan 
