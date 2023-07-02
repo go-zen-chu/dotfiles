0e8a0832e838a4fe67f26c3f104a76048977a636
Akira Masuda <masuda.akira1014@gmail.com>
2023-07-02 17:05:03 +0900

    update aqua settings

--- aqua/setup.sh
+++ aqua/setup.sh
@@ -9,7 +9,8 @@ rm ./aqua-installer
 # temporary add to PATH (add to PATH in zshrc for permanent configuration)
 export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua"
-ln -s ./aqua/aqua.yaml "${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml"
+# create hard link (not symbolic link)
+ln ./aqua/aqua.yaml "${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml"
 # set to global config
 export AQUA_GLOBAL_CONFIG=${AQUA_GLOBAL_CONFIG:-}:${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml
 aqua i -a
