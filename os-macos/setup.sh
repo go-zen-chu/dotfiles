30182470fd2b1505d4bd23a3f30d1f971dc67d66
Akira Masuda <masuda.akira1014@gmail.com>
2025-01-06 18:17:11 +0900

    comment out safari default setting because of permission modified in new macos

--- os-macos/setup.sh
+++ os-macos/setup.sh
@@ -135,9 +135,9 @@ macos_setup_defaults() {
 
     #========================== Safari Settings ==========================
     # Enable the `Develop` menu and the `Web Inspector`
-    defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-    defaults write com.apple.Safari IncludeDevelopMenu -bool true
-    defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+    #defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+    #defaults write com.apple.Safari IncludeDevelopMenu -bool true
+    #defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 
     #========================== iCal Settings ==========================
     # default duration to 15m
