56df1f5683ce8c4de734acbe7bd5312789ddfe5f
Akira Masuda <masuda.akira1014@gmail.com>
2025-01-09 00:11:44 +0900

    update

--- install.sh
+++ install.sh
@@ -357,6 +357,7 @@ setup_personal_machine_tools() {
 linux_setup_personal_machine_tools() {
     echo_blue "Setup personal machine tools for linux..."
 
+    # tailscale
     if hash tailscale 2>/dev/null; then
         log "$LOG_LEVEL_INFO" "[✓] tailscale is already installed"
         return
@@ -364,8 +365,34 @@ linux_setup_personal_machine_tools() {
     log "$LOG_LEVEL_INFO" "[ ] tailscale not installed. Installing..."
     curl -fsSL https://tailscale.com/install.sh | sh
     log "$LOG_LEVEL_INFO" "[✓] tailscale install finished"
-
     echo_green "run 'sudo tailscale up' for joining to tailscale network"
+
+    # xdg-open for tmux-plugin
+    if ! hash xdg-open 2>/dev/null; then
+        log "$LOG_LEVEL_INFO" "[ ] xdg-open not installed. Installing..."
+        linux_package_install "xdg-utils"
+        log "$LOG_LEVEL_INFO" "[✓] xdg-open install finished"
+    fi
+}
+
+linux_package_list_updated="false"
+
+linux_package_install() {
+    local package_name
+    package_name=$1
+
+    case "${os}" in
+    "ubuntu")
+        if [ "${linux_package_list_updated}" = "false" ]; then
+            sudo apt-get update
+            linux_package_list_updated="true"
+        fi
+        sudo apt-get install -y "${package_name}"
+        ;;
+    "*")
+        log "$LOG_LEVEL_ERROR" "skip package install for ${os} because it is not supported"
+        ;;
+    esac
 }
 
 setup_zsh() {
